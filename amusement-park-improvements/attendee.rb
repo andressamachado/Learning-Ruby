e98855005c144d4b844ea27066c4cc1479071e2d
andressamachado <andressa.pamachado@gmail.com>
2022-07-23 23:42:25 -0400

    boolean

---
+++ amusement-park-improvements/attendee.rb
@@ -0,0 +1,34 @@
+class Attendee
+  def initialize(height)
+    @height = height
+    @pass_id = nil
+  end
+  
+  def height
+    @height
+  end
+
+  def pass_id
+    @pass_id
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+  
+  def has_pass?
+    @pass_id != nil 
+  end
+
+  def fits_ride?(ride_minimum_height)
+    @height >= ride_minimum_height 
+  end
+
+  def allowed_to_ride?(ride_minimum_height)
+    self.has_pass? && fits_ride?(ride_minimum_height)
+  end
+end
